4f7d1287e4767879cf44cbcebd3d5775d1520204
AnnikaLaa <annika0096@gmail.com>
2024-06-16 16:00:35 +0300

    added API interaction

--- src/index.js
+++ src/index.js
@@ -2,15 +2,25 @@ let poemFormElement = document.querySelector("#poem-form");
 poemFormElement.addEventListener("submit", generatePoem);
 
 let poemElement = document.querySelector("#poem");
+let input = document.querySelector("#user-input");
+
+let apiKey = "baf15f814713odta8a4baa99ed0733e5";
+
+function displayPoem(response) {
+  let newPoem = response.data.answer;
+  new Typewriter("#poem", {
+    strings: newPoem,
+    autoStart: true,
+    delay: 20,
+    cursor: "",
+  });
+}
 
 function generatePoem(event) {
   event.preventDefault();
-  new Typewriter("#poem", {
-    strings: "Two hearts in tune, a perfect song,",
-    autoStart: true,
-    delay: 100,
-    cursor: "",
-  });
-
-  //poemElement.innerHTML = "generating poem";
+  let prompt = `Write a short sweet rhyming poem about ${input.value}`;
+  let context =
+    "You are a creative poet. Please provide poems in basic HTML, maximum length 500 and separate lines with <br> element. Make sure that first line of answer is first line of poem. Please don`t add titles.";
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+  axios.get(apiUrl).then(displayPoem);
 }
